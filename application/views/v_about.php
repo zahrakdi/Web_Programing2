04d5f30074e59069214c7afcc70f11d561548f51
zahrakdi <kdizahra@gmail.com>
2023-04-04 20:25:17 +0700

    Tambahkan file

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Fauwas</li>
+        <li>Nama Belakang : Abdul Kadir</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Malang</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Kediri</li>
+    </ul>
+    <h4>Hobi</h4>
+    <ul type="square">
+        <li>Makan</li>
+        <li>Tidur</li>
+    </ul>
+</section>
